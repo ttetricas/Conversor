ba1d734b8e8bfe536b56293067d1c61eb2b0b7c2
ttetricas <estermeireles89@gmail.com>
2025-03-31 14:49:23 -0300

    31.03

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Por favor, digite o valor em wons: ");
+string input = Console.ReadLine()!;
+
+double valorWon = Convert.ToDouble(input);
+
+double taxaConversao = 0.0040;
+
+double valorConvertido = valorWon * taxaConversao;
+
+Console.WriteLine("O valor convertido em reais é: R$" + valorConvertido.ToString("F2"));
